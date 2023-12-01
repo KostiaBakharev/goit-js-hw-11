36e59a2e25026a935449495e570a86388c679c55
KostiaBakharev <kostya3698@gmail.com>
2023-12-01 22:56:46 +0200

    психічне порушення

--- src/js/index.js
+++ src/js/index.js
@@ -11,54 +11,13 @@ const refs = {
   gallery: document.querySelector('.gallery'),
 };
 const { form, gallery } = refs;
-
+const btn = document.querySelector('.load-more');
 let query = '';
 let page = 1;
 const perPage = 40;
 
 form.addEventListener('submit', onSubmit);
 
-function createMarkup(array) {
-  if (!gallery) {
-    return;
-  }
-
-  const markup = array
-    .map(image => {
-      const {
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      } = image;
-      return `<div class="gallery__item"
-      <a class="gallery__link" href="${largeImageURL}">
-      <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
-      </div> 
-      <div class="info">
-        <p class="info-item"><b>Likes: </b>${likes}</p>
-        <p class="info-item"><b>Views: </b>${views}</p>
-        <p class="info-item"><b>Comments: </b>${comments}</p>
-        <p class="info-item"><b>Downloads: </b>${downloads}</p>
-        </div>
-      </a>
-    `;
-    })
-    .join('');
-
-  gallery.innerHTML = markup;
-  const lightbox = new SimpleLightbox('.gallery__item', {
-    captionsData: 'alt',
-    captionDelay: 250,
-  });
-}
-
-// s
-
-//
 function onSubmit(evt) {
   evt.preventDefault();
   page = 1;
@@ -79,7 +38,11 @@ function onSubmit(evt) {
       } else {
         Notify.success(`Hooray! We found ${data.totalHits} images.`);
         createMarkup(data.hits);
-        scrollToGallery();
+        // scrollToGallery();
+        lightbox.refresh();
+      }
+      if (data.totalHits > perPage) {
+        window.addEventListener('scroll', scrollToGallery);
       }
     })
     .catch(error => {
@@ -87,6 +50,50 @@ function onSubmit(evt) {
     })
     .finally(() => form.reset());
 }
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function createMarkup(array) {
+  // if (!gallery) {
+  //   return;
+  // }
+
+  const markup = array
+    .map(image => {
+      const {
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      } = image;
+      return `<div class="gallery__item"
+      <a class="gallery__link" href="${largeImageURL}">
+      <img class="gallery__image" src="${webformatURL}" alt="${tags}" width="300" loading="lazy" />
+      
+      <div class="info">
+      <p class="info-item"><b>Likes: </b>${likes}</p>
+      <p class="info-item"><b>Views: </b>${views}</p>
+      <p class="info-item"><b>Comments: </b>${comments}</p>
+      <p class="info-item"><b>Downloads: </b>${downloads}</p>
+      </div></div> 
+      </a>
+      `;
+    })
+    .join('');
+
+  gallery.innerHTML = markup;
+  lightbox.refresh();
+}
+
+// s
+
+//
 // Прокручування сторінки
 function scrollToGallery() {
   const { height: cardHeight } = document
@@ -98,3 +105,38 @@ function scrollToGallery() {
     behavior: 'smooth',
   });
 }
+btn.addEventListener('click', loadMoreImg);
+
+function loadMoreImg() {
+  page += 1;
+  // lightbox.destroy();
+  fetchImages()
+    .then(data => {
+      createMarkup(data.hits);
+      lightbox.refresh();
+
+      const totalPages = Math.round(data.totalHits / perPage);
+
+      gallery.insertAdjacentHTML('beforeend', createMarkup);
+      if (page > totalPages) {
+        Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+        // lightbox.refresh();
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching images:', error);
+    });
+}
+// function checkIfEndOfPage() {
+//   return (
+//     window.innerHeight + window.scrollY >= document.documentElement.scrollHeight
+//   );
+// }
+function showLoadMorePage() {
+  if (checkIfEndOfPage()) {
+    loadMoreImg();
+  }
+}
+// window.addEventListener('scroll', showLoadMorePage);

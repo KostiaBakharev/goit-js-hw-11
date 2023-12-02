00651f52bc51197a12925b9f5783523260d1cf8c
KostiaBakharev <137355180+KostiaBakharev@users.noreply.github.com>
2023-12-02 00:53:23 +0000

    Deploying to gh-pages from @ KostiaBakharev/goit-js-hw-11@86fc1b912e5df9b7085db2a80beb463ebab17199 🚀

---
+++ index.c42d730a.js
@@ -0,0 +1,2 @@
+!function(){var o=window.scrollY;window.onscroll=function(){var n=window.scrollY;document.getElementById("search-form").style.top=o>n?"0":"-80px",o=n}}();
+//# sourceMappingURL=index.c42d730a.js.map
---
+++ index.ffdbe069.js
@@ -0,0 +1,2 @@
+let o=window.scrollY;window.onscroll=function(){const l=window.scrollY;document.getElementById("search-form").style.top=o>l?"0":"-80px",o=l};
+//# sourceMappingURL=index.ffdbe069.js.map
